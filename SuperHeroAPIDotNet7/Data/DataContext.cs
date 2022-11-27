cb577496f99fc340c7422984c52446e94a0ada3d
Serhat403 <serhatoeners97@outlook.de>
2022-11-27 19:00:45 +0100

    First Commit

---
+++ SuperHeroAPIDotNet7/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPIDotNet7.Data
+{
+    public class DataContext: DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("//Here Put your Server");
+            
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
